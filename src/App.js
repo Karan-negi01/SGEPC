d8b0383de72b62f3596debbb4f78ff65d1440f76
Karan Negi <btwitskaranhere@gmail.com>
2024-10-27 00:16:43 +0530

    navigates

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
 import Homepage from "./components/homepage/Homepage";
 import About from "./components/aboutus/About";
 import Awards from "./components/awards/Awards";
-import { useNavigate } from "react-router-dom";
-
 
 function App() {
-
-  const Navigate =(path)=>{
-    Navigate(path)
-  }
-
   return (
     <BrowserRouter>
-    <Routes>
-
-
-    <Route path="/" element={<Navigate to="/home" />} />
-      <Route path="/home" element={<Homepage/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/awards" element={<Awards/>}/>
-      
-      
-      
-
-
-    </Routes>
-
-
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} /> {/* Redirect root to homepage */}
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/awards" element={<Awards />} />
+      </Routes>
     </BrowserRouter>
   );
 }
